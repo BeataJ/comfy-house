77f9b1891032fd815e83c00e28272b47eb09a161
beata <beata@emc2wave.com>
2019-12-25 21:59:27 -0800

    create setCartValue(0

--- app.js
+++ app.js
@@ -78,10 +78,23 @@ class UI {
         // save cart in local storage
         Storage.saveCart(cart);
         // set cart value
+        this.setCartValue(cart);
         // display cart item
         // show the cart
       });
     });
+  }
+
+  setCartValue(cart) {
+    let tempTotal = 0;
+    let itemsTotal = 0;
+    cart.map(item => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
+    console.log(cartTotal, cartItems);
   }
 }
 
