296c1df6a267c39ed7c7f0f6f8788b1cdbef04d0
beata <beata@emc2wave.com>
2019-12-25 21:13:00 -0800

    corect "cart" to cart

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ class Storage {
     return products.find(product => product.id === id);
   }
   static saveCart(cart) {
-    localStorage.setItem("cart", JSON.stringify("cart"));
+    localStorage.setItem("cart", JSON.stringify(cart));
   }
 }
 
